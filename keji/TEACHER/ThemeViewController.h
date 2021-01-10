e2f9a4864d8860acb327f3e35f9c573ef3412633
WXXUPT <1454291663@qq.com>
2021-01-10 21:31:24 +0800

    1.10

--- keji/TEACHER/ThemeViewController.h
+++ keji/TEACHER/ThemeViewController.h
@@ -14,7 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) UIPickerView *pickerView;
 @property (nonatomic, strong) UIButton *cancelButton;
-@property (nonatomic, strong) UIButton *sureButton;1
+@property (nonatomic, strong) UIButton *sureButton;
 @property NSArray *signArray;
 @end
 
